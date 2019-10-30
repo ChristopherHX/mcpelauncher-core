a882b51cf6bca05aee56b712f95851cb8624fe50
Christopher Homberger <christopher.homberger@web.de>
2019-10-30 17:54:46 +0100

    Check if SharedConstants::MajorVersion is NULL Missing from Versions > 1.13.0.6

--- include/mcpelauncher/minecraft_version.h
+++ include/mcpelauncher/minecraft_version.h
@@ -13,14 +13,14 @@ private:
 
 public:
     static bool isAtLeast(int major, int minor, int patch = -1, int revision = -1) {
-        return *SharedConstants::MajorVersion > major || (*SharedConstants::MajorVersion == major &&
+        return !SharedConstants::MajorVersion || *SharedConstants::MajorVersion > major || (*SharedConstants::MajorVersion == major &&
             (*SharedConstants::MinorVersion > minor || (*SharedConstants::MinorVersion == minor &&
             (*SharedConstants::PatchVersion > patch || (*SharedConstants::PatchVersion == patch &&
             Revision() >= revision)))));
     }
 
     static bool isExactly(int major, int minor, int patch, int revision) {
-        return *SharedConstants::MajorVersion == major && *SharedConstants::MinorVersion == minor &&
+        return SharedConstants::MajorVersion && *SharedConstants::MajorVersion == major && *SharedConstants::MinorVersion == minor &&
                 Revision() == revision && *SharedConstants::PatchVersion == patch;
     }
 
